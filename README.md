50c947c910f318ea52022e1d88fc6c90dd3027f5
Pedro Yanez <pedro_y91@outlook.com>
2022-06-05 01:27:27 -0400

    182/animacions

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ### Requisitos
 - Node: 14.19.2
 - NPM: 8.9.0
+- Animate.css 4.1.1=> https://animate.style/
--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,12 +1,12 @@
+import { useMemo } from "react";
 import { useParams, Navigate, useNavigate } from "react-router-dom"
 import { getHeroByID } from "../../selectors/getHeroById";
-
 
 export const HeroScreen = () => {
   const { heroeId } = useParams();
   const navigate = useNavigate();
 
-  const hero = getHeroByID(heroeId);
+  const hero = useMemo(() => getHeroByID(heroeId), [heroeId]);
 
   if (!hero) {
     return <Navigate to="/" />
@@ -30,11 +30,11 @@ export const HeroScreen = () => {
 
     <div className='row mt-5'>
       <div className='col-md-4'>
-        <img src={imgPath} alt={superhero} className='img-thumbnail' />
+        <img src={imgPath} alt={superhero} className='img-thumbnail animate__animated animate__fadeInLeft' />
       </div>
       <div className="col-8">
         <h3>{superhero}</h3>
-        <ul className="list-group list-group-flush">
+        <ul className="list-group list-group-flush animate__animated animate__fadeIn">
           <li className="list-group-item"><b>Publisher:</b> {publisher}</li>
           <li className="list-group-item"><b>Alter-ego:</b> {alter_ego}</li>
           <li className="list-group-item"><b>First appearance:</b> {first_appearance}</li>
