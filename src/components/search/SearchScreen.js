dbc0ea0555121279cacb1ac9b8e14e5b99a468b8
Pedro Yanez <pedro_y91@outlook.com>
2022-06-05 01:46:20 -0400

    183/Seach Component

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,7 +1,50 @@
+import { useForm } from "../../hooks/useForm";
+
 export const SearchScreen = () => {
+
+  const [formValues, handleInputChange] = useForm({
+    searchText: '',
+  })
+
+  const { searchText } = formValues;
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(searchText)
+
+  }
+
   return (
-    <div>
-      <h1>SearchScreen</h1>
-    </div>
+    <>
+      <h1>Busqueda</h1>
+      <hr />
+
+      <div className="row">
+
+        <div className="col-5">
+          <h4>Buscar</h4>
+          <hr />
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Buscar"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={handleInputChange}
+            />
+
+            <button
+              type="submit"
+              className="btn btn-outline-primary mt-1"
+            >
+              Buscar
+            </button>
+          </form>
+        </div>
+
+      </div>
+    </>
   )
 }
