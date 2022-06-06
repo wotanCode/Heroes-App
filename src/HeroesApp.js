0aa897b2831939ac53940dfb78bd8fe93482ef56
Pedro Yanez <pedro_y91@outlook.com>
2022-06-05 20:36:47 -0400

    193/proveer estado global

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,7 +1,23 @@
 import { AppRouter } from "./routers/AppRouter";
 
+import { AuthContext } from "./auth/authContext";
+import { useReducer } from "react";
+import { authReducer } from "./auth/authReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('user')) || { logged: false };
+}
+
 export const HeroesApp = () => {
+
+  const [user, dispatch] = useReducer(authReducer, {}, init);
+
   return (
-    <AppRouter />
+    <AuthContext.Provider value={{
+      user,
+      dispatch
+    }}>
+      <AppRouter />
+    </AuthContext.Provider>
   );
 }
