aee6231530c8fe6c81f8d060e5c4ccb9f7135ad1
Pedro Yanez <pedro_y91@outlook.com>
2022-06-05 20:14:26 -0400

    192/Context y reducer

---
+++ src/auth/authReducer.js
@@ -0,0 +1,26 @@
+import { types } from "../types/types";
+
+// const state = {
+//   name: 'Pedro',
+//   logged: true,
+// };
+
+export const authReducer = (state = {}, action) => {
+
+  switch (action.type) {
+    case types.login:
+      return {
+        name: action.payload.name,
+        ...action.payload,
+      }
+
+    case types.logout:
+      return {
+        logged: false,
+      }
+
+    default:
+      return state;
+  }
+
+}
