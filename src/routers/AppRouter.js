231449fc766a38b0039231c697e36b92757a3ce4
Pedro Yanez <pedro_y91@outlook.com>
2022-06-05 21:27:42 -0400

    197/rutas publicas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,13 +3,21 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoutes } from "./PublicRoutes";
 
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginScreen />} />
+
+        <Route path="/login" element=
+          {
+            <PublicRoutes>
+              <LoginScreen />
+            </PublicRoutes>
+          }
+        />
 
         <Route path="/*" element=
           {
